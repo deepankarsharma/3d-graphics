3b066b823a45ce284b5f8756662244e33c8f0742
Deepankar Sharma <deepankarsharma@gmail.com>
2025-04-30 01:51:39 -0400

    Updated code

--- src/main.c
+++ src/main.c
@@ -14,13 +14,12 @@ int initialize_window(void) {
 
   // Create a SDL window
   window = SDL_CreateWindow(
-                                      NULL,
-                                      SDL_WINDOWPOS_CENTERED,
-                                      SDL_WINDOWPOS_CENTERED,
-                                      800,
-                                      600,
-                                      SDL_WINDOW_BORDERLESS        
-                            );
+                            NULL,
+                            SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED,
+                            800,
+                            600,
+                            SDL_WINDOW_BORDERLESS);
 
   if (!window) {
     fprintf(stderr, "Error creating SDL window. \n");
@@ -37,7 +36,47 @@ int initialize_window(void) {
   return true;
 }
 
+
+void setup(void) {
+}
+
+void process_input(void) {
+  SDL_Event event;
+  SDL_PollEvent(&event);
+
+  switch (event.type) {
+    case SDL_QUIT:
+      is_running = false;
+      break;
+  case SDL_KEYDOWN:
+    if (event.key.keysym.sym == SDLK_ESCAPE) {
+      is_running = false;
+    }
+    break;
+  }
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_RenderPresent(renderer);
+}
+
 int main(void) {
   is_running = initialize_window();
+
+  setup();
+
+  while (is_running) {
+    process_input();
+    update();
+    render();
+  }
+
   return 0;
 }
